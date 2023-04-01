89ef8073483be4fd906bb650b8c37f3709f3e73b
Pavel <pavlik1123.pg@gmail.com>
2023-04-01 23:15:08 +0700

    add di-java

---
+++ src/main/java/org/example/config/JavaConfig.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import org.example.controller.PostController;
+import org.example.repository.PostRepository;
+import org.example.service.PostService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    public PostController postController(PostService service) {
+        return new PostController(service);
+    }
+
+    @Bean
+    public PostService postService(PostRepository repository) {
+        return new PostService(repository);
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepository();
+    }
+}
